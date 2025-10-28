ef9979434fecb29826f06a16a362d926db70666c
Wesley Unwin <wes_unwin@hotmail.com>
2025-10-28 09:51:44 -0400

    Fix comment about setOngoing

--- src/android/CallNotification.java
+++ src/android/CallNotification.java
@@ -89,7 +89,7 @@ public class CallNotification {
             .setLargeIcon(BitmapFactory.decodeResource(this.context.getResources(), android.R.drawable.sym_def_app_icon))
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setCategory(NotificationCompat.CATEGORY_CALL)
-            .setOngoing(true); // Can't be "dismissed" by user must action it
+            .setOngoing(true); // Can't be "dismissed" by the user, app will handle closing it
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             Log.d(TAG, "Creating CallStyle.forIncomingCall style notification (as this is supported by the device)...");
