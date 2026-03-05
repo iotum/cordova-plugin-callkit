3e6ab7626da35c132c46372008cb27dfa7eefbbc
Wesley Unwin <wes_unwin@hotmail.com>
2026-03-05 17:55:01 -0500

    MyConnectionService: remove logic disconnecting existing ringing connections, on another incoming call

--- src/android/MyConnectionService.java
+++ src/android/MyConnectionService.java
@@ -114,17 +114,6 @@ public class MyConnectionService extends ConnectionService {
                         Bundle callInfo = new Bundle();
                         callInfo.putString("payload", payloadString);
 
-                        // For robustness (avoiding violating MAX_RINGING_CALLS) + due to limitations of the current UI:
-                        // End any existing or lingering ringing connections before calling addNewIncomingCall() as it would fail:
-                        // (event: onCreateIncomingCallFailed reason: MAX_RINGING_CALLS)
-                        for (String key : connectionMap.keySet()) {
-                            Connection conn = connectionMap.get(key);
-                            if (conn.getState() == Connection.STATE_RINGING) {
-                                Log.d(TAG, "Disconnecting existing ringing connection for call_uuid: " + key + " before adding new call");
-                                conn.setDisconnected(new DisconnectCause(DisconnectCause.LOCAL)); // Connection will later be destroyed + removed (see connection.onStateChanged).
-                            }
-                        }
-
                         Log.d(TAG, "Adding new incoming connection, callUUID: " + callUUID);
 
                         // After this a new connection is created (see onCreateIncomingConnection below)
