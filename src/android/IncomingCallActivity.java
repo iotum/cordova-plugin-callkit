97b436f69d7b90feb80a487c493ec9d5cc733747
Wesley Unwin <wes_unwin@hotmail.com>
2026-03-10 09:22:51 -0400

    IncomingCallActivity: ensure CallStateReceiver checks session id of broadcast to sessionid of the ringing call

--- src/android/IncomingCallActivity.java
+++ src/android/IncomingCallActivity.java
@@ -33,14 +33,16 @@ public class IncomingCallActivity extends AppCompatActivity {
 
     private int callerNameViewID;
 
+    private String sessionId; // sessionId of the ringing call associated with this acivity
+
     private final BroadcastReceiver callStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             Log.d(TAG, "callStateReciever onReceive: " + action);
-            if ("connection_state_changed".equals(action)) {
-                Connection conn = MyConnectionService.getConnection(intent.getStringExtra("sessionId"));
-                if (conn.getState() == Connection.STATE_DISCONNECTED) {
+            if ("connection_state_changed".equals(action) && sessionId.equals(intent.getStringExtra("sessionId"))) {
+                int newState = intent.getIntExtra("state", 0);
+                if (newState == Connection.STATE_DISCONNECTED) {
                     Log.d(TAG, "closing activity as call was disconnected");
                     finishAndRemoveTask();
                 }
@@ -52,12 +54,14 @@ public class IncomingCallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        this.sessionId = this.getIntent().getStringExtra("sessionId");
+
         IntentFilter filter = new IntentFilter("connection_state_changed");
 
         Log.d(TAG, "Registering callStateReciever");
         LocalBroadcastManager.getInstance(this.getApplicationContext()).registerReceiver(callStateReceiver, filter);
 
-        String sessionId = this.getIntent().getStringExtra("sessionId");
+
 
         Connection connection = MyConnectionService.getConnection(sessionId);
 
@@ -176,6 +180,8 @@ public class IncomingCallActivity extends AppCompatActivity {
     public void onNewIntent(@NonNull Intent intent, @NonNull ComponentCaller caller) {
         super.onNewIntent(intent, caller);
         this.setIntent(intent); // So that future calls to this.getIntent() return the new intent, and not the initial intent of the activity
+
+        this.sessionId = intent.getStringExtra("sessionId");
 
         this.updateCallerNameView();
     }
