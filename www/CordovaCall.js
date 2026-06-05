3e312621bd7d9f7b03014c7776a85a0b82154341
Jerry Hu <jerry@iotum.com>
2026-06-05 08:40:29 -0400

    fix error handling

--- www/CordovaCall.js
+++ www/CordovaCall.js
@@ -32,7 +32,7 @@ exports.setIncludeInRecents = function (value, success, error) {
   if (typeof value == "boolean") {
     exec(success, error, "CordovaCall", "setIncludeInRecents", [value]);
   } else {
-    error("Value Must Be True Or False");
+    if (typeof error == "function") { error("Value Must Be True Or False"); }
   }
 };
 
@@ -40,7 +40,7 @@ exports.setDTMFState = function (value, success, error) {
   if (typeof value == "boolean") {
     exec(success, error, "CordovaCall", "setDTMFState", [value]);
   } else {
-    error("Value Must Be True Or False");
+    if (typeof error == "function") { error("Value Must Be True Or False"); }
   }
 };
 
@@ -48,7 +48,7 @@ exports.setAllowUnmute = function (sessionId, value, success, error) {
   if (typeof value == "boolean") {
     exec(success, error, "CordovaCall", "setAllowUnmute", [sessionId, value]);
   } else {
-    error("Value Must Be True Or False");
+    if (typeof error == "function") { error("Value Must Be True Or False"); }
   }
 };
 
@@ -56,7 +56,7 @@ exports.setVideo = function (value, success, error) {
   if (typeof value == "boolean") {
     exec(success, error, "CordovaCall", "setVideo", [value]);
   } else {
-    error("Value Must Be True Or False");
+    if (typeof error == "function") { error("Value Must Be True Or False"); }
   }
 };
 
